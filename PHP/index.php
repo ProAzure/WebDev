42bc7ed6592aca6038fc6f90fd1086b908a81d60
Atharva3749 <atharvashinde64@gmail.com>
2024-03-28 19:04:03 +0530

    note app backend created

--- PHP/index.php
+++ PHP/index.php
@@ -1,3 +1,11 @@
+<?php  
+$conn=mysqli_connect('localhost','root','','notes');
+if($conn){
+  echo "the connection done successfully";
+}else{
+  echo "something went wrong";
+}
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -6,7 +14,7 @@
     <title>Notes --Notes taking Application</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
-  <body class="bg-dark">
+  <body class="bg-dark text-white">
    <header class="mt-2 mx-4">
     <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
         <div class="container-fluid">
@@ -31,20 +39,61 @@
       </nav>
    </header>
    <!-- header ends here  -->
-
+   <?php 
+   $title=$_GET['noteTitle'];
+    $desc=$_GET['noteDesc'];
+    $Sql="INSERT INTO `user`(`title`, `description`) VALUES ('$title','$desc')";
+    $result=mysqli_query($conn,$Sql);
+    if($result){
+      echo "The query is inserted successfully";
+    }
+   ?>
     <div class="container text-white">
-        <form>
+        <form method="GET">
             <div class="mb-3">
               <label for="notesTitle" class="form-label">Title</label>
-              <input type="text" class="form-control" id="notesTitle" aria-describedby="emailHelp">
+              <input type="text" class="form-control" name="noteTitle" id="notesTitle" aria-describedby="emailHelp">
             </div>
             <div class="mb-3">
                 <label for="exampleFormControlTextarea1" class="form-label">Description</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <textarea class="form-control" name="noteDesc" id="exampleFormControlTextarea1" rows="3"></textarea>
               </div>
             <button type="submit" class="btn btn-primary">Add note</button>
           </form>
     </div>
+    <div class="container text-white mt-3">
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Sno</th>
+      <th scope="col">Note</th>
+      <th scope="col">Description </th>
+      <th scope="col">Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+    $sql="SELECT * FROM `user`";
+    $result=mysqli_query($conn,$sql);
+    $sno=1;
+    while($data=mysqli_fetch_assoc($result)){
+
+      echo "<tr>
+      <th>".$sno."</th>
+      <td>".$data['title']."</td>
+      <td>".$data['description']."</td>
+      <td><button>Edit</button> <button>Delete</button></td>
+    </tr>";
+    $sno=$sno+1;
+;
+    }
+    ?>
+  
+  </tbody>
+</table>
+    </div>
+
+  
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </html>
